8e2eec769c0c9ef37ed4907f569f74a9b03c3444
Noah Fernandes <>
2018-04-11 18:54:16 +0200

    Added Circle graphic

--- src/modules/GraphicsModule/index.js
+++ src/modules/GraphicsModule/index.js
@@ -4,6 +4,7 @@ const TimelineLite = require('gsap/TimelineLite');
 const utils = require('../../utils');
 
 const Arc = require('./Arc');
+const Circle = require('./Circle');
 const Line = require('./Line');
 const Ring = require('./Ring');
 
@@ -20,34 +21,22 @@ class GraphicsModule {
     return new Ring(x, y, options);
   }
 
-  drawCircle(x, y) {
-    const graphic = new PIXI.Graphics();
-    graphic.cacheAsBitmapboolean = true;
-
-    const baseRadius = 30 + utils.getRandom(40);
-
-    graphic.lineStyle(utils.getRandom(5), 0x000000);
-    graphic.beginFill(0x000000);
-
-    graphic.drawCircle(x, y, radius);
-
-    graphic.endFill();
-
-    return graphic;
+  drawCircle(x, y, options) {
+    return new Circle(x, y, options);
   }
 
   drawRandom(x, y, options) {
-    let random = utils.getRandom(3);
+    let random = utils.getRandom(4);
 
     switch (random) {
       case 1:
         return this.drawArc(x, y, options);
       case 2:
-        return this.drawLine(x, y, options);
-      case 3:
-        return this.drawRing(x, y, options);
-      case 4:
         return this.drawCircle(x, y, options);
+      case 3:
+        return this.drawLine(x, y, options);
+      case 4:
+        return this.drawRing(x, y, options);
     }
   }
 }
